bd0979d711adbce5593b72e032036237c9ae9ddb
skynetgarden <skynetgarden@gmail.com>
2018-10-22 22:29:43 +0700

    fix-22-10-18

---
+++ node-sbadmin/controller/api/view-history.js
@@ -0,0 +1,64 @@
+var request = require('request');
+
+module.exports.view_product = function(req, res) {
+	
+		var productid=req.param('orgid_key');
+		var options = {
+  			url: 'http://18.136.205.13:3000/api/v1/products/'+ productid,
+  			method: 'GET',
+		};
+		
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode==200){
+			var data=[JSON.parse(body)];
+			res.render('template/api/view-history',{data:data,message:"",status:0});
+		}else if(!error && response.statusCode!=200){
+			res.render('template/api/view-history',{data:'',message:'No data found!',status:2});
+		}else{
+			res.render('template/api/view-history',{data:'',message:error,status:2});
+		}
+     	});
+};
+
+/* ///////////////////////// */
+
+module.exports.product_api004 = function(req, res) {
+	
+		var productid=req.params.productid;
+		var options = {
+  			url: 'http://18.136.205.13:3000/api/v1/products/'+ productid +'/logs',
+  			method: 'GET',
+		};
+		
+	request(options, function (error, response, body) {
+	res.send(body);
+     	});
+};
+
+/* ///////////////////////// */
+
+module.exports.views_products = function(req, res) {
+	
+		var productid=req.params.productid;
+		var options = {
+  			url: 'http://18.136.205.13:3000/api/v1/products/',
+  			method: 'GET',
+		};
+		
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode==200){
+			var data=JSON.parse(body);
+			res.render('template/api/view-history',{data:data,message:"",status:0});
+		}else if(!error && response.statusCode!=200){
+			res.render('template/api/view-history',{data:'',message:'No data found!',status:2});
+		}else{
+			res.render('template/api/view-history',{data:'',message:error,status:2});
+		}
+     	});
+};
+
+/* ///////////////////////// */
+
+
+
+
