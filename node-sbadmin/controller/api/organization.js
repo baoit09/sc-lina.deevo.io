4e93ac40dca747cfb1eb68ac08b7cca608dda01c
Hoa <huypham25101996@gmail.com>
2018-10-23 15:52:37 +0700

    Add View Detail

--- node-sbadmin/controller/api/organization.js
+++ node-sbadmin/controller/api/organization.js
@@ -30,10 +30,12 @@ module.exports.org_api003 = function(req, res) {
   		method: 'GET',
 	};
 
+
 	request(options, function (error, response, body) {
 		if(!error && response.statusCode==200){
-			var data=[JSON.parse(body)];
-			res.render('template/api/organization',{data:data,message:"",status:0});
+			refresh;
+			// var data=[JSON.parse(body)];
+			// res.render('template/api/organization',{data:data,message:"",status:0});
 		}else if(!error && response.statusCode!=200){
 			res.render('template/api/organization',{data:'',message:"Không tìm thấy yêu cầu !!!",status:2});
 		}else{
@@ -100,7 +102,21 @@ module.exports.org_api002 = function(req,res) {
 
 	request(options, function (error, response, body) {
 		if(!error && response.statusCode==200){
-			res.render('template/api/organization',{data:'',message:"Thực hiện thành công !!!",status:1});
+			var options = {
+				url: 'http://18.136.205.13:3000/api/v1/orgs',
+				method: 'GET',
+				};
+		
+			request(options, function (error, response, body) {
+				if(!error && response.statusCode==200){
+					var refresh=JSON.parse(body);
+					res.render('template/api/organization',{data:refresh,message:"Thực hiện thành công !!!",status:1});
+				}else if(!error && response.statusCode!=200){
+					res.render('template/api/organization',{data:'',message: JSON.stringify(response),status:2});
+				}else{
+					res.render('template/api/organization',{data:'',message:error,status:2});
+				}
+			});
 		}else if(!error && response.statusCode!=200){
 			res.render('template/api/organization',{data:'',message: JSON.stringify(response),status:2});
 		}else{
